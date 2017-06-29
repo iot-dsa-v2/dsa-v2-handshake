f7a15196dd95d0b8b61f915fdb16000eb2fe00df
Ben Richards <blrichards@me.com>
2017-06-29 18:41:45 +0000

    fixed install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 if [ ! -d build ]; then
 	mkdir build
 fi
-cmake . -DCMAKE_INSTALL_PREFIX=. -B./build
+cmake . -DBoost_USE_STATIC_LIBS=ON -DCMAKE_INSTALL_PREFIX=. -B./build
 cd build
 # make
 make install
