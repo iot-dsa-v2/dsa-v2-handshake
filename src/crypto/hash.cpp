d80c31ffb1460ed37e044450344f4363425b54c1
Ben Richards <blrichards@me.com>
2017-06-30 12:12:28 -0700

    fixed bad throw calls

--- src/crypto/hash.cpp
+++ src/crypto/hash.cpp
@@ -10,11 +10,11 @@
 dsa::hash::hash(const char *hash_type) : finalized(false) {
   const EVP_MD *md = EVP_get_digestbyname(hash_type);
   if (md == nullptr)
-    throw("invalid hash type");
+    throw std::runtime_error("invalid hash type");
   mdctx = EVP_MD_CTX_create();
   EVP_MD_CTX_init(mdctx);
   if (EVP_DigestInit_ex(mdctx, md, nullptr) <= 0)
-    throw("something went wrong initializing digest");
+    throw std::runtime_error("something went wrong initializing digest");
 }
 
 dsa::hash::~hash() {
@@ -27,7 +27,7 @@ void dsa::hash::update(std::vector<byte> data) {
 
 std::string dsa::hash::digest_base64() {
   if (finalized)
-    throw("digest already called");
+    throw std::runtime_error("digest already called");
 
   unsigned char *md_value = new unsigned char[EVP_MAX_MD_SIZE];
   unsigned int md_len;
@@ -42,7 +42,7 @@ std::string dsa::hash::digest_base64() {
 std::string dsa::gen_salt(int len) {
   unsigned char buf[len];
   if (!RAND_bytes(buf, len))
-    throw("Unable to generate salt");
+    throw std::runtime_error("Unable to generate salt");
   std::string out = reinterpret_cast<char *>(buf);
   out[len] = '\0';
   return out;
